3e104ecef06c7cbc0b10493dcf5a8f9ad87663a0
TCC_Grupo_04 <marianaetecprojetos@gmail.com>
2025-09-28 03:11:17 -0300

    Add files via upload

--- home.php
+++ home.php
@@ -11,11 +11,75 @@
 
 <body>
     <h1>Relp!</h1>
-    <h2>Streamline</h2>
-    <div class="box">
-        <a href="login.php">Acesso Empresa (CEO/Funcionário)</a>
-        <a href="formulario.php">Cadastre sua Empresa</a>
-    </div>
-</body>
+<div class="box box-login">
+  <a href="login.php">Login</a>
+</div>
 
+<div class="box box-fornecedor">
+  <a href="login_fornecedor.php">Acesso Fornecedor</a>
+</div>
+
+<div class="box box-cadastro">
+  <a href="formulario.php">Cadastro</a>
+</div>
+
+<h3>RELP!<br>
+FOR YOUR COMPANY</h3>
+
+<div class="Quem">
+    <!-- Botão que leva até a div "info" -->
+    <a href="#info" class="botaoQ">Quem somos</a>
+  </div class="boxDoQuemsomos">
+    <div>
+      <div id="info" class="conteudo">
+      <h2>Quem somos?</h2>
+      <p>Como a Streamline pode te ajudar via RELP!</p>
+    </div>
+
+    <div class="imagem">
+      <img src="img/logostreamline.png">
+    </div>
+  </div>
+
+  <div class="Servicos">
+    <!-- Botão que leva até a div "info" -->
+    <a href="#info2" class="botaoS">Serviços</a>
+  </div>
+
+  <div id="info2" class="conteudo2">
+    <h2>Nossas principais funcionalidades </h2>
+    <p>Idealizadas e programadas pensando ans saus necessidades diárias como MEI, <br>CEO, funcionário e até fornecedor de uma micro empresa nos dias atuais...</p>
+  </div>
+<div class="FAQ">
+    <!-- Botão que leva até a div "info" -->
+    <a href="#info3" class="botaoF">FAQ</a>
+  </div>
+
+  <div id="info3" class="conteudo3">
+    <h2>Nossa equipe de desenvolvedores <br>vem da startup StreamLine, que <br>projetou a RELP! para facilitar sua <br>vida.</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum. Sed <br>accumsan quam vitae est varius fringilla. Pellentesque placerat vestibulum lorem sed <br>porta. Nullam mattis tristique iaculis. Nullam pulvinar sit amet risus pretium auctor. Etiam <br>quis massa pulvinar, aliquam quam vitae, tempus sem. Donec elementum pulvinar odio.</p>
+  </div>
+
+  <div class="imagem2">
+      <img src="img/imagemfuncionalidades.png">
+    </div>
+
+  <div class="imagem3">
+      <img src="img/imagemhomecomeco.jpeg">
+    </div>
+
+  <div class="imagem4">
+      <img src="img/imagemplanossemfundo.png">
+    </div>
+
+    <div id="info4" class="conteudo4">
+      <h2>Planos disponíveis para compra</h2>
+      <p>Pensados de forma acessível para atender suas principais necessidades de forma <br>prática e democratica.</p>
+    </div>
+
+    
+
+
+
+</body>
 </html>
